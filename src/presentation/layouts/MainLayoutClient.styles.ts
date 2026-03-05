dd18635fa9f670495f9e5e4e893a0c77a08d2f54
Angélica Silva Leite <angelica.leite@cebraspe.org.br>
2026-03-05 20:03:03 -0300

    style: ajustes

--- src/presentation/layouts/MainLayoutClient.styles.ts
+++ src/presentation/layouts/MainLayoutClient.styles.ts
@@ -1,6 +1,6 @@
 export const mainLayoutClientClasses = {
   root: "min-h-screen bg-background",
   desktopSidebarWrapper: "hidden md:block",
-  main: "min-h-screen pt-16 px-4 pb-6 md:pt-0 md:ml-64 md:p-8",
-  content: "mx-auto max-w-6xl",
+  main: "min-h-screen pt-16 px-4 pb-6 md:ml-64 md:p-8",
+  content: "max-w-6xl",
 } as const;
